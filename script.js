403ac275e027a5a3a203feb621ec3a2142a8be33
Jerome <jerome.dev392@gmail.com>
2024-05-25 19:40:50 +0800

    Created a function that compares each weapon of choice Displaying each player weapon of choice and scoring records The scoring resets when a player reaches 5 points

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+const scores = {
+    human: 0,
+    computer: 0
+}
+
+function resetScores() {
+    scores.human = scores.computer = 0;
+}
+
 function getComputerChoice() {
     var choice = Math.floor(Math.random() * 3) + 1;
     return choice;
 }
 
 function getWeapon(humanChoice, computerChoice) {
-    let array = [humanChoice, computerChoice]
+    let array = [humanChoice, computerChoice];
     for (let index = 0; index < array.length; index++) {
         switch (array[index]) {
             case 1:
@@ -19,16 +28,34 @@ function getWeapon(humanChoice, computerChoice) {
             default:
                 array[index] = 'invalid';
                 break;
-        }
-    }
+        };
+    };
 
     return array;
 }
 
 function compareResult(weapons) {
-    
+    const outcomes = {
+        rock: 'scissors',
+        scissors: 'paper',
+        paper: 'rock'
+    };
+
+    if (weapons[0] === weapons[1]) {
+        return "draw";
+    } else {
+        console.log(weapons)
+        return outcomes[weapons[0]] === weapons[1] ? (scores.human++, 'Human wins!')
+            : (scores.computer++, 'Computer wins!');
+    }
 }
 
 function playRound(selected) {
-    compareResult(getWeapon(selected, getComputerChoice()));
+    console.log(compareResult(getWeapon(selected, getComputerChoice())));
+    console.log(scores);
+
+    if (scores.human === 5 || scores.computer === 5) {
+        console.log(`${scores.human === 5 ? 'Human' : 'Computer'} wins!`);
+        resetScores();
+    }
 }
